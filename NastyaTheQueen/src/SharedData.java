8ea7efa3b7eab35dfc11e3e5edca17b7775c75cf
nastya53 <nastia53@gmail.com>
2022-10-30 12:55:18 +0200

    jrjrrjr

--- NastyaTheQueen/src/SharedData.java
+++ NastyaTheQueen/src/SharedData.java
@@ -9,32 +9,49 @@ public class SharedData
 	private boolean flag;
 	private final int b;
 	
+	/**
+	 * @param array
+	 * @param getting informatin
+	 */
 	public SharedData(ArrayList<Integer> array, int b) {
 		
 		this.array = array;
 		this.b = b;
 	}
 
+	/**
+	 * @return the wining array
+	 */
 	public boolean[] getWinArray() 
 	{
 		return winArray;
 	}
+
 
 	public void setWinArray(boolean [] winArray) 
 	{
 		this.winArray = winArray;
 	}
 
+	/**
+	 * @return the array list
+	 */
 	public ArrayList<Integer> getArray() 
 	{
 		return array;
 	}
 
+	/**
+	 * @return the number 
+	 */
 	public int getB() 
 	{
 		return b;
 	}
 
+	/**
+	 * @return the flag
+	 */
 	public boolean getFlag() 
 	{
 		return flag;
